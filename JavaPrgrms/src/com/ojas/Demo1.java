1b46707a6f6e34e32198b2fd539cf22d32ea563d
afreed-shaik <afreed405shaik@gmail.com>
2021-05-12 23:14:40 +0530

    commit

--- JavaPrgrms/src/com/ojas/Demo1.java
+++ JavaPrgrms/src/com/ojas/Demo1.java
@@ -1,6 +1,5 @@
 package com.ojas;
 
-
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,11 +8,10 @@ import java.util.Scanner;
 
 
 
-public class Demo {
+public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection(
@@ -21,21 +19,28 @@ public class Demo {
 			System.out.println("Connected" + con);
 			Scanner scan = new Scanner(System.in);
 			System.out.println("Enter empno : ");
-			CallableStatement cst = con.prepareCall("{CALL getSal(?,?)}");
-			cst.setInt(1, scan.nextInt());
-			cst.registerOutParameter(2, Types.FLOAT);
+			CallableStatement cst = con.prepareCall("{?=CALL getAverage(?,?)}");
+			
+			cst.registerOutParameter(1, Types.FLOAT);
+			cst.setInt(2, scan.nextInt());
+			cst.setInt(3, scan.nextInt());
 			cst.execute();
-			float salary = cst.getFloat(2);
-			System.out.println("Your Salary is = " + salary);
+			float salary = cst.getFloat(1);
+			System.out.println("Your Average Salary is = " + salary);
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
 	}
+	
 //	delimiter //
-//	mysql> create procedure getSal(in id int, out sal int)
-//	    -> begin
-//	    -> select salary into sal from employees where empno=id;
+//	mysql> create function getAverage(id1 int, id2 int) returns int DETERMINISTIC
+//	    -> BEGIN
+//	    -> declare sal1 int;
+//	    -> declare sal2 int;
+//	    -> select salary into sal1 from employees where empno=id1;
+//	    -> select salary into sal2 from employees where empno=id2;
+//	    -> return (sal1+sal2)/2;
 //	    -> end;
 //	    -> //
 
